ba528a6019426620ad6577b931c73f89ae32f66f
Scott Render <scottjrender@gmail.com>
2024-02-07 19:40:18 +0000

    Restart, basic sfml

--- sim/Main.cpp
+++ sim/Main.cpp
@@ -1,17 +1,12 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
-#include "Element.h"
-
-int WIDTH = 400;
-int HEIGHT = 400;
+const int WIDTH = 400;
+const int HEIGHT = 400;
 
 int main()
 {
-    std::cout << "hello world\n";
-
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "particle-sim");
-    
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -20,14 +15,5 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        window.clear(sf::Color::Black);
-        window.display();
     }
-
-    std::cout << "sfml success!\n";
-
-    Element element; 
-
-    return 0;
 }
